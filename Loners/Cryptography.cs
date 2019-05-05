4eb6e91315395adac7efd81b639d9ec943c0217e
Martin Timothy Timko <45509732+mttcode@users.noreply.github.com>
2019-05-05 21:20:31 +0200

    Create Cryptography.cs

---
+++ Loners/Cryptography.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+
+namespace Loners
+{
+    public static class Cryptography
+    {
+        public static string Hash(string value)
+        {
+            return Convert.ToBase64String(System.Security.Cryptography.SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(value)));
+        }
+    }
+}
